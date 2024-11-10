5f7ed8bfbd6d3f1c0aac79ccfcf2cf1856c58d14
tetyana-zorya <tetyanazorya@gmail.com>
2024-11-09 17:44:50 -0800

    adding base nuxt

---
+++ gearx/pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const router = useRouter()
+
+const pages = [{
+  name: 'Home',
+  link: '/'
+},
+  {
+    name: 'Resrouces',
+    link: '/resources'
+  }
+]
+</script>
+
+<template>
+  <div class="flex justify-center p-8" >
+    <div>Whatcom Outdoor Alliance</div>
+  </div>
+</template>
